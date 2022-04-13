17e49bc9cc07155a1fc164c119b153d7ea0ddaeb
billy <billygr@users.noreply.github.com>
2022-04-13 14:44:43 +0300

    Update environment-main.tf

--- environment-main.tf
+++ environment-main.tf
@@ -9,7 +9,11 @@ terraform {
 
 # Configuration of Terraform with Azure environment variables
 provider "azurerm" {
-  features { }
+  features { 
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 #  client_id           = var.azure-client-id
 #  client_secret       = var.azure-client-secret
 #  subscription_id     = var.azure-subscription
