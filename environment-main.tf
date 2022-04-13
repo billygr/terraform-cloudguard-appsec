942301e2e669f751bd9d16918fd004e5860234f1
billygr <billygr@users.noreply.github.com>
2022-04-13 12:41:41 +0300

    Simply deployment

--- environment-main.tf
+++ environment-main.tf
@@ -10,10 +10,10 @@ terraform {
 # Configuration of Terraform with Azure environment variables
 provider "azurerm" {
   features { }
-  client_id           = var.azure-client-id
-  client_secret       = var.azure-client-secret
-  subscription_id     = var.azure-subscription
-  tenant_id           = var.azure-tenant
+#  client_id           = var.azure-client-id
+#  client_secret       = var.azure-client-secret
+#  subscription_id     = var.azure-subscription
+#  tenant_id           = var.azure-tenant
 }
 
 # Creation of DNS Zone
--- environment-variables.tf
+++ environment-variables.tf
@@ -1,23 +1,23 @@
-variable "azure-client-id" {
-    description = "Insert your application client-id"
-    type = string
-    sensitive = true    
-} 
-variable "azure-client-secret" {
-    description = "Insert your application client-secret"
-    type = string
-    sensitive = true
-}
-variable "azure-subscription" {
-    description = "Insert your subscription-id"
-    type = string
-    sensitive = true
-}
-variable "azure-tenant" {
-    description = "Insert your active-directory-id"
-    type = string
-    sensitive = true
-}
+#variable "azure-client-id" {
+#    description = "Insert your application client-id"
+#    type = string
+#    sensitive = true    
+#} 
+#variable "azure-client-secret" {
+#    description = "Insert your application client-secret"
+#    type = string
+#    sensitive = true
+#}
+#variable "azure-subscription" {
+#    description = "Insert your subscription-id"
+#    type = string
+#    sensitive = true
+#}
+#variable "azure-tenant" {
+#    description = "Insert your active-directory-id"
+#    type = string
+#    sensitive = true
+#}
 variable "location" {
     description = "Choose where to deploy the environment"
     type = string
